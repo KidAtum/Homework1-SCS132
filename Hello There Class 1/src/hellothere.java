c69cf19fd9b9ed4b37754a2b75c4becda0876c30
lucasweakland <lucasweakland@10.242.2.68>
2019-09-05 16:39:24 -0400

    Ready for review.

---
+++ Hello There Class 1/src/hellothere.java
@@ -0,0 +1,18 @@
+
+public class hellothere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Project Name: Hello There 1
+		
+		System.out.println("Oh my gosh I love this class.");
+		System.out.println("Liv lets play mario kart and skip classes");
+		System.out.print("This is line one. \n\tThis is line two ");
+		System.out.println("\n\tThat V8 energy drink was surprisingly good!");
+		
+		
+		
+		
+	}
+
+}
---
+++ Hello There Class 1/src/scs132homework1.java
@@ -0,0 +1,24 @@
+// Project:	Homework 1 SCS132
+// Author: Lucas M. Weakland
+
+public class scs132homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wow I really love this class!");
+		System.out.println("Thanks again for the V8 energy drink!");
+		System.out.println("I think next time im going to have a redbull though");
+		System.out.println("We can sip on some energy drinks and make more fun of JJ");
+		System.out.println("I still cant believe JJ had a baby");
+		System.out.println("You can call me Uncle Louie");
+		System.out.println("Does Uncle Louie sound like a creepy name though?");
+		System.out.println("Anyways, I think ill be a good uncle");
+		System.out.println("I just have to figure out how to properly hold a baby");
+		System.out.println("Well, thank you for reading. Can't wait to see what else this class has to offer!");
+		
+		
+		
+		
+	}
+
+}
